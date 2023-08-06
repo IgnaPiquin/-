72e3d7cb55c47da316f97c04d3f74b4df6972089
IgnaPiquin <ignaciopiquin@gmail.com>
2023-08-06 16:28:09 -0300

    .

--- FirstPracticeProyect/main.py
+++ FirstPracticeProyect/main.py
@@ -1,11 +1,21 @@
 import sqlite3
+import pandas as pd
 from Categories import *
 
-with sqlite3.connect('FirstPracticeProyect.db') as connection:
-    repository = CategoryRepository(connection)
 
-    name = ""
-    description = ""
+def add_category(name, description):
+    with sqlite3.connect('FirstPracticeProyect.db') as connection:
+        repository = CategoryRepository(connection)
 
-    add_category = AddCategory('Categories', 'CategoryName', 'CategoryDescription', name, description, repository)
-    add_category.Add_Category()
+        add_category = AddCategory('Categories', 'CategoryName', 'CategoryDescription', name, description, repository)
+        add_category.Add_Category()
+
+
+
+
+
+consult = input('1 - Add Category')
+if consult == '1':
+    name = input('Input the name you want to use with the new category: ')
+    description = input('Input the description you want to add to the new category: ')
+    add_category(name, description)
