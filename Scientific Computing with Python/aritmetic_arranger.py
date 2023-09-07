93750199d53fd2d5ace0924ca50ba4947d021018
IgnaPiquin <ignaciopiquin@gmail.com>
2023-09-07 09:40:11 -0300

    .

--- Scientific Computing with Python/aritmetic_arranger.py
+++ Scientific Computing with Python/aritmetic_arranger.py
@@ -1,19 +1,28 @@
 import re
 def arithmetic_arranger(problems, answer=False):
-  
+#running through problems
   for i in range(len(problems)):
-        problem = problems[i]
-        numbers = re.findall('[0-9]+', problem)
-        length = 0
-        for number in numbers:
+      problem = problems[i]
+      #finding the numbers of the ecuation
+      numbers = re.findall('[0-9]+', problem)
+      #finding the largest number
+      length = 0
+      for number in numbers:
           if length < len(number):
             length = len(number)
-        
-        for i in range(len(numbers)):
-            numbers[i] = int(numbers[i])
-        sing = re.findall('[+-]+', problem)
-        
-        print(numbers, sing, length)
+      #changing the str variable to a int variable
+      # for i in range(len(numbers)):
+      #       numbers[i] = int(numbers[i])
+      #finding the sing of the problem
+      sing = re.findall('[+-]+', problem)
+      # alining the numbers 
+      ecuation=f'''{numbers[0]:>{length+2}}
+{sing[0]} {numbers[1]:>{length}}\n'''
+      # adding the slashes to the ecuation
+      slashes = '-' * (length+2)
+      ecuation = ecuation + slashes
+      print (ecuation)
+
       
       
   
